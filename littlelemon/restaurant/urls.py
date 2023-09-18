49e45a6f4898e9d4c56ffca62befb75daf01ebbb
Fabian Ayala <fabianjoseayalavargas@gmail.com>
2023-09-18 18:48:00 -0400

    Authentication Security Added

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,12 @@
-from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import index, MenuView, BookingView, MenuItemsView, SingleMenuItemView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('booking/', BookingView.as_view()),
+    path('menu-items', MenuView.as_view()),
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token)
 ]
